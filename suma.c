1c4db90605d9e03ff2d22388a78fabd08a9d75a8
kokotr <AntonioPerezap077@gmail.com>
2021-11-26 15:51:14 -0600

    primer commit

---
+++ suma.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+int main(){
+
+int a,b;
+
+system("clear");
+printf("PROGRAMA QUE REALIZA SUMAS CON 2 DIJITOS\n");
+printf("Dame un numero?:");
+scanf("%d",&a);
+
+printf("Dame otro numero?:");
+scanf("%d",&b);
+
+
+
+printf(" :) yes el resultado de la suma es: %d",a+b);
+
+
+
+return 0;
+}
+
+
